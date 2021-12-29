1fda5af298e72dc8d278eaaca71a06a4b2bb4f93
Peter Bell <peter.bell215@gmail.com>
2021-12-29 08:52:10 +0000

    Improvements in Coordinate class building on improved Angle class

---
+++ lib/great-circle/coordinate.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'active_support/core_ext'
+
+# Class to represents a location on the earth using latitude and longitude.
+class Coordinate
+  include Math
+
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude:, longitude:)
+    @latitude = Latitude.new(latitude)
+    @longitude = Longitude.new(longitude)
+
+    @vincenty_solutions = {}
+  end
+
+  def latitude=(val)
+    @latitude = val.is_a?(Latitude) ? val : Latitude.new(val)
+  end
+
+  def longitude=(val)
+    @longitude = val.is_a?(Longitude) ? val : Longitude.new(val)
+  end
+
+  def distance_to(final_coordinate)
+    find_or_calc_vincenty_solution(final_coordinate).distance
+  end
+
+  def initial_heading_to(final_coordinate)
+    find_or_calc_vincenty_solution(final_coordinate).initial_bearing
+  end
+
+  def final_heading_from(start_coordinate)
+    find_or_calc_vincenty_solution(start_coordinate).final_bearing
+  end
+
+  # Given a heading and bearing from the current position, returns a new position on a great circle based on
+  # the initial heading.
+  # rubocop: disable Metrics/AbcSize PB: no easy way to simplify this method given complex maths involved
+  def new_position(heading:, distance:)
+    delta = Angle.new(radians: distance.to_f / Vincenty::WGS84_A)
+
+    _new_latitude = new_latitude(delta, heading)
+    _new_longitude = new_longitude(delta, heading, _new_latitude)
+
+    Coordinate.new(latitude: _new_latitude, longitude: _new_longitude)
+  end
+  # rubocop: enable Metrics/AbcSize
+
+  # Comparison operator for Coordinate
+  def eql?(other)
+    @latitude == other.latitude && @longitude == other.longitude
+  end
+  alias == eql?
+
+  private
+
+  def new_latitude(delta, heading)
+    new_latitude_in_radians = asin(self.latitude.sin * delta.cos + self.latitude.cos * delta.sin * heading.cos)
+    Latitude.new(radians: new_latitude_in_radians)
+  end
+
+  def new_longitude(delta, heading, new_latitude)
+    new_longitude_in_radians =
+      self.longitude.radians + atan2(heading.sin * delta.sin * self.latitude.cos, delta.cos - self.latitude.sin * new_latitude.sin)
+    new_longitude = Longitude.new(radians: new_longitude_in_radians)
+  end
+
+
+
+  def find_or_calc_vincenty_solution(final_coordinate)
+    @vincenty_solutions[final_coordinate] ||= Vincenty.iterative_solver(self, final_coordinate)
+  end
+end
--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -11,112 +11,17 @@ describe Coordinate do
     let(:coord_50_minus5) { Coordinate.new(latitude: 50, longitude: -5) }
     let(:coord_58_minus3) { Coordinate.new(latitude: 58, longitude: -3) }
     let(:coord_minus58_3) { Coordinate.new(latitude: -58, longitude: 3) }
-    let(:coord_10_10) { Coordinate.new(latitude: 10, longitude: 10) }
-    let(:coord_50_50) { Coordinate.new(latitude: 50, longitude: 50) }
+    let(:coord_10_10) { Coordinate.new(latitude: 10.degrees, longitude: 10.degrees) }
+    let(:coord_50_50) { Coordinate.new(latitude: 50.degrees, longitude: 50.degrees) }
   end
   # rubocop: enable RSpec/MultipleMemoizedHelpers
   # rubocop: enable Naming/VariableNumber
 
   describe 'initialization' do
     it 'initializes object with provided parameters' do
-      new_coordinate = Coordinate.new(latitude: 10,
-                                      longitude: 20)
-      expect(new_coordinate.latitude).to eq(10)
-      expect(new_coordinate.longitude).to eq(20)
-    end
-  end
-
-  describe 'coordinate parsing' do
-    it 'accepts a simple string for latitudes and longitudes' do
-      coordinate.latitude = '-10'
-      coordinate.longitude = '10'
-      expect(coordinate.latitude).to eq(-10.0)
-      expect(coordinate.longitude).to eq(10.0)
-    end
-
-    it 'accepts a decimal string for latitudes and longitudes' do
-      coordinate.latitude = '-10.5'
-      coordinate.longitude = '10.5'
-      expect(coordinate.latitude).to eq(-10.5)
-      expect(coordinate.longitude).to eq(10.5)
-    end
-
-    describe 'with cardinal directions' do
-      it 'saves the latitude if given N as cardinal direction' do
-        coordinate.latitude = '10.5 N'
-        expect(coordinate.latitude).to eq(10.5)
-      end
-
-      it 'makes the latitude negative if given S as a cardinal direction' do
-        coordinate.latitude = '10.5 S'
-        expect(coordinate.latitude).to eq(-10.5)
-      end
-
-      it 'handles spaces around things' do
-        coordinate.latitude = '  10.5   N  '
-        expect(coordinate.latitude).to eq(10.5)
-      end
-
-      it 'throws an exception if given E or W as a direction for latitude' do
-        expect { coordinate.latitude = '10 E' }.to raise_error(ArgumentError)
-        expect { coordinate.latitude = '10 W' }.to raise_error(ArgumentError)
-      end
-
-      it 'saves the longitude if given E as a cardinal direction' do
-        coordinate.longitude = '10.5 E'
-        expect(coordinate.longitude).to eq(10.5)
-      end
-
-      it 'makes the longitude negative if given W as a cardinal direction' do
-        coordinate.longitude = '10.5 W'
-        expect(coordinate.longitude).to eq(-10.5)
-      end
-
-      it 'throws an exception if given N or S as a direction for longitude' do
-        expect { coordinate.longitude = '10 N' }.to raise_error(ArgumentError)
-        expect { coordinate.longitude = '10 S' }.to raise_error(ArgumentError)
-      end
-    end
-
-    describe 'parsing degrees minutes and seconds' do
-      it 'takes simple degrees input and set as decimal' do
-        coordinate.latitude = '50°'
-        expect(coordinate.latitude).to eq(50.0)
-
-        coordinate.longitude = '10.5 °'
-        expect(coordinate.longitude).to eq(10.5)
-      end
-
-      it 'takes degrees and minutes' do
-        coordinate.latitude = "50° 0'"
-        expect(coordinate.latitude).to eq(50)
-
-        coordinate.latitude = '50° 0′'
-        expect(coordinate.latitude).to eq(50)
-      end
-
-      it 'converts minutes to 1/60ths of degrees' do
-        coordinate.latitude = "50° 30'"
-        expect(coordinate.latitude).to eq(50.5)
-      end
-
-      it 'takes degrees, minutes, and seconds' do
-        coordinate.latitude = "50° 0' 0\""
-        expect(coordinate.latitude).to eq(50)
-
-        coordinate.latitude = "50° 0' 0″"
-        expect(coordinate.latitude).to eq(50)
-      end
-
-      it 'takes degrees and seconds' do
-        coordinate.latitude = '50° 0"'
-        expect(coordinate.latitude).to eq(50)
-      end
-
-      it 'converts seconds to 1/60ths of minutes' do
-        coordinate.latitude = "50° 0' 36\""
-        expect(coordinate.latitude).to eq(50.01)
-      end
+      new_coordinate = Coordinate.new(latitude: 10, longitude: 20)
+      expect(new_coordinate.latitude).to eq(10.degrees)
+      expect(new_coordinate.longitude).to eq(20.degrees)
     end
   end
 
@@ -132,38 +37,6 @@ describe Coordinate do
     it { is_expected.not_to be_eql(different_longitude) }
   end
 
-  describe 'validation' do
-    it 'is valid if given valid lat and long' do
-      expect(coordinate).to be_valid
-    end
-
-    it 'is invalid unless latitude provided' do
-      coordinate.latitude = nil
-      expect(coordinate).not_to be_valid
-    end
-
-    it 'is invalid unless longitude provided' do
-      coordinate.longitude = nil
-      expect(coordinate).not_to be_valid
-    end
-
-    it 'is invalid unless latitude is between -90 and 90' do
-      coordinate.latitude = -100
-      expect(coordinate).not_to be_valid
-
-      coordinate.latitude = 100
-      expect(coordinate).not_to be_valid
-    end
-
-    it 'is invalid unless longitude is between -180 and 180' do
-      coordinate.longitude = -190
-      expect(coordinate).not_to be_valid
-
-      coordinate.longitude = 190
-      expect(coordinate).not_to be_valid
-    end
-  end
-
   describe '#new_position' do
     include_context 'with common coordinates for testing'
 
@@ -173,8 +46,8 @@ describe Coordinate do
       let(:distance) { start_position.distance_to(end_position) }
       let(:heading) { start_position.initial_heading_to(end_position) }
 
-      specify { expect(new_position.latitude).to be_within(0.3).of(end_position.latitude) }
-      specify { expect(new_position.longitude).to be_within(0.3).of(end_position.longitude) }
+      specify { expect(new_position.latitude.radians).to be_within(0.3).of(end_position.latitude.radians) }
+      specify { expect(new_position.longitude.radians).to be_within(0.3).of(end_position.longitude.radians) }
     end
 
     it_behaves_like 'calculation based on heading and distance' do
